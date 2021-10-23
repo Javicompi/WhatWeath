8fd2413074acb6cc26f1a8a4884f2e9acdcabf81
Javicompi <javicompi@gmail.com>
2021-10-23 12:10:30 +0200

    Removed unused imports

--- domain/src/main/java/es/jnsoft/domain/usecase/GetCurrentByIdUseCase.kt
+++ domain/src/main/java/es/jnsoft/domain/usecase/GetCurrentByIdUseCase.kt
@@ -2,9 +2,7 @@ package es.jnsoft.domain.usecase
 
 import es.jnsoft.domain.model.Current
 import es.jnsoft.domain.repository.CurrentRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetCurrentByIdUseCase @Inject constructor(
