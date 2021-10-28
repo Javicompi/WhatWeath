7af0b884ecf8b34bc3fb6e42ec3c057f85757448
Javicompi <javicompi@gmail.com>
2021-10-28 18:55:16 +0200

    Added new values for upcoming API calls

--- framework/src/main/java/es/jnsoft/framework/remote/service/ApiConstants.kt
+++ framework/src/main/java/es/jnsoft/framework/remote/service/ApiConstants.kt
@@ -7,4 +7,8 @@ object ApiConstants {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
     const val API_KEY = BuildConfig.API_KEY
+
+    const val EXCLUDES_HOURLY = "current,minutely,daily,alerts"
+
+    const val EXCLUDES_DAILY = "current,minutely,hourly,alerts"
 }
