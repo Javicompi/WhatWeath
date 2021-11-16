659e3c305b7f64c1c24a8fe17c08614d1d97792b
Javicompi <javicompi@gmail.com>
2021-11-16 17:20:19 +0100

    Replaced BaseViewModel

--- app/src/main/java/es/jnsoft/whatweath/presentation/ui/base/BaseViewModel.kt
+++ app/src/main/java/es/jnsoft/whatweath/presentation/ui/base/BaseViewModel.kt
@@ -1,44 +1,12 @@
 package es.jnsoft.whatweath.presentation.ui.base
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import es.jnsoft.domain.enums.Units
 import es.jnsoft.domain.usecase.GetUnitsUseCase
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.launch
 
-abstract class BaseViewModel<DomainData, PresentationData>(
+
+abstract class BaseViewModel(
     getUnitsUseCase: GetUnitsUseCase
 ) : ViewModel() {
 
-    protected val _domainData = MutableStateFlow<DomainData?>(null)
-    val domainData: StateFlow<DomainData?> = _domainData
-
-    private val eventChannel = Channel<Event>(Channel.BUFFERED)
-    val eventsFlow = eventChannel.receiveAsFlow()
-
-    fun sendEvent(event: Event) {
-        viewModelScope.launch {
-            eventChannel.send(event)
-        }
-    }
-
-    private val units = getUnitsUseCase.invoke(Unit)
-
-    val presentation = combine(_domainData, units) { domainData, units ->
-        mapToPresentation(domainData = domainData, units = units)
-    }
-
-    abstract fun mapToPresentation(domainData: DomainData?, units: Units): PresentationData?
-
-    sealed class Event {
-        data class NavigateTo(val destination: String): Event()
-        data class ShowSnackbarResource(val resource: Int): Event()
-        data class ShowSnackbarString(val message: String): Event()
-        object Clean: Event()
-    }
+    protected val units = getUnitsUseCase.invoke(Unit)
 }
