20f841b10eea422a3a980ab4520de786fad7be95
Javicompi <javicompi@gmail.com>
2021-11-17 17:36:41 +0100

    Implemented CoroutineWorker initialization into Application

--- app/src/main/java/es/jnsoft/whatweath/WhatWeathApplication.kt
+++ app/src/main/java/es/jnsoft/whatweath/WhatWeathApplication.kt
@@ -1,8 +1,50 @@
 package es.jnsoft.whatweath
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.*
 import dagger.hilt.android.HiltAndroidApp
+import es.jnsoft.whatweath.work.CurrentUpdateWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
 
 @HiltAndroidApp
-class WhatWeathApplication : Application() {
+class WhatWeathApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    private val applicationScope = CoroutineScope(Dispatchers.Main)
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        applicationScope.launch {
+            delayedInit()
+        }
+    }
+
+    private fun delayedInit() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
+            .build()
+        val repeatingRequest =
+            PeriodicWorkRequestBuilder<CurrentUpdateWorker>(60, TimeUnit.MINUTES)
+                .setConstraints(constraints)
+                .build()
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            CurrentUpdateWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+    }
 }
