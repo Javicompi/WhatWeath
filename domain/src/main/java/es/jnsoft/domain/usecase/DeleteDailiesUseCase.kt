956e5fb927b938fd6181932a8b128b1260b4fde9
Javicompi <javicompi@gmail.com>
2021-11-27 13:22:50 +0100

    Implemented Daily UseCases

---
+++ domain/src/main/java/es/jnsoft/domain/usecase/DeleteDailiesUseCase.kt
@@ -0,0 +1,16 @@
+package es.jnsoft.domain.usecase
+
+import es.jnsoft.domain.model.Daily
+import es.jnsoft.domain.repository.DailyRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class DeleteDailiesUseCase @Inject constructor(
+    private val repository: DailyRepository
+) : BaseUseCase<List<Daily>, Unit> {
+
+    override suspend fun invoke(params: List<Daily>) = withContext(Dispatchers.IO) {
+        repository.deleteDailies(params)
+    }
+}
---
+++ domain/src/main/java/es/jnsoft/domain/usecase/FindDailiesUseCase.kt
@@ -0,0 +1,18 @@
+package es.jnsoft.domain.usecase
+
+import es.jnsoft.domain.model.Daily
+import es.jnsoft.domain.model.Location
+import es.jnsoft.domain.model.Result
+import es.jnsoft.domain.repository.DailyRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class FindDailiesUseCase @Inject constructor(
+    private val repository: DailyRepository
+) : BaseUseCase<Location, Result<List<Daily>>> {
+
+    override suspend fun invoke(params: Location) = withContext(Dispatchers.IO) {
+        repository.findDailies(params.lat, params.lon)
+    }
+}
