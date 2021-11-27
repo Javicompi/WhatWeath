f4da84dd44011c3c9e796655961d5c303c90048a
Javicompi <javicompi@gmail.com>
2021-11-27 13:10:46 +0100

    Implemented DailyRepository.kt

---
+++ domain/src/main/java/es/jnsoft/domain/repository/DailyRepository.kt
@@ -0,0 +1,20 @@
+package es.jnsoft.domain.repository
+
+import es.jnsoft.domain.model.Daily
+import es.jnsoft.domain.model.Result
+import kotlinx.coroutines.flow.Flow
+
+interface DailyRepository {
+
+    fun getDailies(lat: Double, long: Double): Flow<List<Daily>>
+
+    suspend fun saveDailies(dailies: List<Daily>)
+
+    suspend fun deleteDailies(dailies: List<Daily>)
+
+    suspend fun findDailies(lat: Double, lon: Double): Result<List<Daily>>
+
+    suspend fun updateDailies(lat: Double, lon: Double)
+
+    fun shouldUpdate(deltaTime: Long): Boolean
+}
