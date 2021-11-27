b271d27828a6d9687d11f069cfa07d9f016df8bd
Javicompi <javicompi@gmail.com>
2021-11-27 13:33:26 +0100

    Implemented DailyData.kt

---
+++ data/src/main/java/es/jnsoft/data/model/DailyData.kt
@@ -0,0 +1,26 @@
+package es.jnsoft.data.model
+
+data class DailyData(
+    val clouds: Int,
+    val deltaTime: Long,
+    val description: String,
+    val dewPoint: Double,
+    val humidity: Int,
+    val icon: String,
+    val lat: Double,
+    val lon: Double,
+    val moonPhase: Double,
+    val pop: Double,
+    val pressure: Int,
+    val rain: Double,
+    val snow: Double,
+    val sunrise: Long,
+    val sunset: Long,
+    val tempMax: Double,
+    val tempMin: Double,
+    val timeZone: Int,
+    val uvi: Double,
+    val visibility: Int,
+    val windDegrees: Int,
+    val windSpeed: Double
+) : BaseData
