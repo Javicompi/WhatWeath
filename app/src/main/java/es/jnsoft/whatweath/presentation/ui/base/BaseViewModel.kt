2908316ef1294f8b24edb6a12d3a252dd9c475f4
Javicompi <javicompi@gmail.com>
2021-12-11 12:34:35 +0100

    Implemented currentDomain into BaseViewModel

--- app/src/main/java/es/jnsoft/whatweath/presentation/ui/base/BaseViewModel.kt
+++ app/src/main/java/es/jnsoft/whatweath/presentation/ui/base/BaseViewModel.kt
@@ -1,12 +1,22 @@
 package es.jnsoft.whatweath.presentation.ui.base
 
 import androidx.lifecycle.ViewModel
+import es.jnsoft.domain.usecase.GetCurrentByIdUseCase
+import es.jnsoft.domain.usecase.GetSelectedIdUseCase
 import es.jnsoft.domain.usecase.GetUnitsUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flatMapLatest
 
-
+@ExperimentalCoroutinesApi
 abstract class BaseViewModel(
-    getUnitsUseCase: GetUnitsUseCase
+    getUnitsUseCase: GetUnitsUseCase,
+    getSelectedIdUseCase: GetSelectedIdUseCase,
+    private val getCurrentByIdUseCase: GetCurrentByIdUseCase
 ) : ViewModel() {
 
     protected val units = getUnitsUseCase.invoke(Unit)
+
+    val currentDomain = getSelectedIdUseCase.invoke(Unit).flatMapLatest { id ->
+        getCurrentByIdUseCase.invoke(id)
+    }
 }
