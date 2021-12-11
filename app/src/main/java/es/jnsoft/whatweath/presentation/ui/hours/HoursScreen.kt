3bd5cf6daaea80ba3afd9a5e09f2acfb2addd198
Javicompi <javicompi@gmail.com>
2021-12-11 12:38:07 +0100

    Removed unnecessary HoursScreen

--- app/src/main/java/es/jnsoft/whatweath/presentation/ui/hours/HoursScreen.kt
+++
@@ -1,9 +0,0 @@
-package es.jnsoft.whatweath.presentation.ui.hours
-
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun HoursScreen() {
-    Text(text = "Hello HoursScreen!")
-}
