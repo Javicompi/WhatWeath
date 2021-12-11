d76379869d01d590ba3ffe0471ecc9239d677928
Javicompi <javicompi@gmail.com>
2021-12-11 12:44:25 +0100

    Removed unnecessary screens and changed icons

--- app/src/main/java/es/jnsoft/whatweath/presentation/ui/main/WhatWeathNavGraph.kt
+++ app/src/main/java/es/jnsoft/whatweath/presentation/ui/main/WhatWeathNavGraph.kt
@@ -4,9 +4,8 @@ import androidx.annotation.StringRes
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,17 +19,14 @@ import es.jnsoft.whatweath.presentation.ui.current.CurrentScreen
 import es.jnsoft.whatweath.presentation.ui.current.CurrentViewModel
 import es.jnsoft.whatweath.presentation.ui.days.DaysScreen
 import es.jnsoft.whatweath.presentation.ui.days.DaysViewModel
-import es.jnsoft.whatweath.presentation.ui.hours.HoursScreen
-import es.jnsoft.whatweath.presentation.ui.search.SearchViewModel
 import es.jnsoft.whatweath.presentation.ui.search.SearchScreen
+import es.jnsoft.whatweath.presentation.ui.search.SearchViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 sealed class BottomNavScreen(val route: String, @StringRes val resourceId: Int, val icon: ImageVector) {
-    object Current : BottomNavScreen("current", R.string.screen_current, Icons.Filled.Home)
-    object Hours : BottomNavScreen("hours", R.string.screen_hours, Icons.Filled.Info)
-    object Days : BottomNavScreen("days", R.string.screen_days, Icons.Filled.Clear)
+    object Current : BottomNavScreen("current", R.string.screen_current, Icons.Filled.Place)
+    object Days : BottomNavScreen("days", R.string.screen_week, Icons.Filled.List)
     object Search : BottomNavScreen("search", R.string.screen_search, Icons.Filled.Search)
-    object Result: BottomNavScreen("search", R.string.screen_search, Icons.Filled.Search)
 }
 
 @ExperimentalCoroutinesApi
@@ -49,9 +45,6 @@ fun WhatWeathNavGraph(
         composable(BottomNavScreen.Current.route) {
             val currentViewModel: CurrentViewModel = hiltViewModel()
             CurrentScreen(currentViewModel)
-        }
-        composable(BottomNavScreen.Hours.route) {
-            HoursScreen()
         }
         composable(BottomNavScreen.Days.route) {
             val daysViewModel: DaysViewModel = hiltViewModel()
