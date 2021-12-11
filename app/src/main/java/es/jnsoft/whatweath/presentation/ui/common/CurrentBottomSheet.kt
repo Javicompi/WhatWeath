79dea66942b1ce4581077159cefb200910e11bf0
Javicompi <javicompi@gmail.com>
2021-12-11 12:33:14 +0100

    Removed unused imports

--- app/src/main/java/es/jnsoft/whatweath/presentation/ui/common/CurrentBottomSheet.kt
+++ app/src/main/java/es/jnsoft/whatweath/presentation/ui/common/CurrentBottomSheet.kt
@@ -1,6 +1,5 @@
 package es.jnsoft.whatweath.presentation.ui.common
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -9,10 +8,8 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
